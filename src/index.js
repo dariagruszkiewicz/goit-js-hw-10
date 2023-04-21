93c059bca8d6bc6925f226136f78c5510784321a
Daria Gruszkiewicz <dariaa.pajak@gmail.com>
2023-04-21 22:43:54 +0200

    update 2
    update 2

--- src/index.js
+++ src/index.js
@@ -1,28 +1,42 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
-import { Notiflix } from 'notiflix';
+import { Notify } from 'notiflix';
+
+Notify.init({
+  width: '300px',
+  position: 'center-top',
+  closeButton: false,
+});
 
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.getElementById('search-box');
 const countryListEl = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 inputEl.addEventListener('input', ev => {
   fetchCountries(ev.target.value)
     .then(countries => renderCountryList(countries))
     .catch(error => console.log(error));
-
-  // if (countries.length > 10) {
-  //   Notify.info('Too many matches found. Please enter a more specific name.');
-  // } else {
-  //   renderCountryList(countries);
-  // }
 });
 
 function renderCountryList(countries) {
-  const markup = countries
-    .map(item => {
-      return `<li><img width="24" src="${item.flags.svg}" alt="${item.flags.alt}"/><p>${item.name.official}</p></li>`;
-    })
-    .join('');
-  countryListEl.innerHTML = markup;
+  if (countries.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  } else if (countries.length >= 1 && countries.length <= 10) {
+    const markup = countries
+      .map(item => {
+        return `<li><img width="24" src="${item.flags.svg}" alt="${item.flags.alt}"/> ${item.name.official}</li>`;
+      })
+      .join('');
+    countryListEl.innerHTML = markup;
+  }
+
+  if (countries.length === 1) {
+    const markup = countries.map(item => {
+      return `<p><b>Capital:</b> ${item.capital}</p>
+          <p><b>Population:</b> ${item.population}</p>
+          <p><b>Languages:</b> ${Object.values(item.languages).join(', ')}</p>`;
+    });
+    countryInfo.innerHTML = markup;
+  }
 }
